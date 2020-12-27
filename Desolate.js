f4fc8964f209d1c0d06234cee82eee60169983fe
smileyy39 <74601585+smileyy39@users.noreply.github.com>
2020-12-27 07:55:10 +0000

    Update Desolate.js

--- Desolate.js
+++ Desolate.js
@@ -1,1 +1,18 @@
+var scriptName = "Desolate"
+var scriptDescription = "Astolfo HvH Script"
 
+function Desolate() {
+    this.onEnable = function() {
+        // on enable
+    }
+}
+
+var module = new Desolate();
+
+this.onEnable = function() {
+    moduleManager.registerModule(module);
+}
+
+this.onDisable = function() {
+    moduleManager.unregisterModule(module);
+}
